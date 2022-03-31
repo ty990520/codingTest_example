25bad8850b32f65b637c5bea64f18d703bbd6531
ty990520 <ty990520@naver.com>
2022-03-31 23:57:56 +0900

    0331

--- src/com/company/notes/twoPointerAndSlidingWindow/Ex4.java
+++ src/com/company/notes/twoPointerAndSlidingWindow/Ex4.java
@@ -1,25 +1,41 @@
-//package com.company.notes.twoPointerAndSlidingWindow;
-//
-//import com.company.Main;
-//
-//import java.util.Scanner;
-//
-//public class Ex4 {
-//    public int solution(int size, int number, int[] arr){
-//
-//    }
-//
-//    public static void main(String[] args) {
-//        Main T= new Main();
-//        Scanner scan = new Scanner(System.in);
-//        int n = scan.nextInt();
-//        int m = scan.nextInt();
-//        int[] arr = new int[m];
-//        for (int i = 0; i < m; i++) {
-//            arr[i] = scan.nextInt();
-//        }
-//        for (int i : T.solution(n,m,arr)) {
-//            System.out.print(i+" ");
-//        }
-//    }
-//}
+package com.company.notes.twoPointerAndSlidingWindow;
+
+import com.company.Main;
+
+import java.util.Scanner;
+
+public class Ex4 {
+    public int solution(int size, int number, int[] arr){
+        int result =0;
+        int sum = 0;
+        int lt = 0;
+
+        for (int rt = 0; rt < size; rt++) {
+            sum+=arr[rt];
+            if(sum==number){
+                result++;
+            }
+            while (sum>=number){
+                sum-=arr[lt];
+                lt++;
+                if(sum==number){
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Ex4 T= new Ex4();
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+        }
+        System.out.println(T.solution(n,m,arr));
+    }
+}
